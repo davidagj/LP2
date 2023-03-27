bda6393672bb696eb92545543f2ffb2143fb21f1
David Aurelio gabilan Junior <87284298+davidagj@users.noreply.github.com>
2023-03-27 19:59:07 -0300

    Add files via upload

---
+++ atividade4/atividade4/atividade4/Form1.Designer.cs
@@ -0,0 +1,157 @@
+﻿namespace atividade4
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtLadoA = new System.Windows.Forms.TextBox();
+            this.txtLadoB = new System.Windows.Forms.TextBox();
+            this.txtLadoC = new System.Windows.Forms.TextBox();
+            this.lblLadoA = new System.Windows.Forms.Label();
+            this.lblLadoB = new System.Windows.Forms.Label();
+            this.lblLadoC = new System.Windows.Forms.Label();
+            this.btnCalcular = new System.Windows.Forms.Button();
+            this.btnLimpar = new System.Windows.Forms.Button();
+            this.btnSair = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtLadoA
+            // 
+            this.txtLadoA.Location = new System.Drawing.Point(246, 47);
+            this.txtLadoA.Name = "txtLadoA";
+            this.txtLadoA.Size = new System.Drawing.Size(100, 20);
+            this.txtLadoA.TabIndex = 0;
+            this.txtLadoA.Validated += new System.EventHandler(this.txtLadoA_Validated);
+            // 
+            // txtLadoB
+            // 
+            this.txtLadoB.Location = new System.Drawing.Point(246, 89);
+            this.txtLadoB.Name = "txtLadoB";
+            this.txtLadoB.Size = new System.Drawing.Size(100, 20);
+            this.txtLadoB.TabIndex = 1;
+            this.txtLadoB.Validated += new System.EventHandler(this.txtLadoB_Validated);
+            // 
+            // txtLadoC
+            // 
+            this.txtLadoC.Location = new System.Drawing.Point(246, 136);
+            this.txtLadoC.Name = "txtLadoC";
+            this.txtLadoC.Size = new System.Drawing.Size(100, 20);
+            this.txtLadoC.TabIndex = 2;
+            this.txtLadoC.Validated += new System.EventHandler(this.txtLadoC_Validated);
+            // 
+            // lblLadoA
+            // 
+            this.lblLadoA.AutoSize = true;
+            this.lblLadoA.Location = new System.Drawing.Point(182, 54);
+            this.lblLadoA.Name = "lblLadoA";
+            this.lblLadoA.Size = new System.Drawing.Size(37, 13);
+            this.lblLadoA.TabIndex = 4;
+            this.lblLadoA.Text = "lado A";
+            // 
+            // lblLadoB
+            // 
+            this.lblLadoB.AutoSize = true;
+            this.lblLadoB.Location = new System.Drawing.Point(182, 96);
+            this.lblLadoB.Name = "lblLadoB";
+            this.lblLadoB.Size = new System.Drawing.Size(41, 13);
+            this.lblLadoB.TabIndex = 5;
+            this.lblLadoB.Text = "Lado B";
+            // 
+            // lblLadoC
+            // 
+            this.lblLadoC.AutoSize = true;
+            this.lblLadoC.Location = new System.Drawing.Point(182, 143);
+            this.lblLadoC.Name = "lblLadoC";
+            this.lblLadoC.Size = new System.Drawing.Size(41, 13);
+            this.lblLadoC.TabIndex = 6;
+            this.lblLadoC.Text = "Lado C";
+            // 
+            // btnCalcular
+            // 
+            this.btnCalcular.Location = new System.Drawing.Point(175, 266);
+            this.btnCalcular.Name = "btnCalcular";
+            this.btnCalcular.Size = new System.Drawing.Size(75, 23);
+            this.btnCalcular.TabIndex = 8;
+            this.btnCalcular.Text = "Calcular";
+            this.btnCalcular.UseVisualStyleBackColor = true;
+            this.btnCalcular.Click += new System.EventHandler(this.btnCalcular_Click);
+            // 
+            // btnLimpar
+            // 
+            this.btnLimpar.Location = new System.Drawing.Point(357, 266);
+            this.btnLimpar.Name = "btnLimpar";
+            this.btnLimpar.Size = new System.Drawing.Size(75, 23);
+            this.btnLimpar.TabIndex = 9;
+            this.btnLimpar.Text = "Limpar";
+            this.btnLimpar.UseVisualStyleBackColor = true;
+            this.btnLimpar.Click += new System.EventHandler(this.btnLimpar_Click);
+            // 
+            // btnSair
+            // 
+            this.btnSair.Location = new System.Drawing.Point(538, 266);
+            this.btnSair.Name = "btnSair";
+            this.btnSair.Size = new System.Drawing.Size(75, 23);
+            this.btnSair.TabIndex = 10;
+            this.btnSair.Text = "Sair";
+            this.btnSair.UseVisualStyleBackColor = true;
+            this.btnSair.Click += new System.EventHandler(this.btnSair_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnSair);
+            this.Controls.Add(this.btnLimpar);
+            this.Controls.Add(this.btnCalcular);
+            this.Controls.Add(this.lblLadoC);
+            this.Controls.Add(this.lblLadoB);
+            this.Controls.Add(this.lblLadoA);
+            this.Controls.Add(this.txtLadoC);
+            this.Controls.Add(this.txtLadoB);
+            this.Controls.Add(this.txtLadoA);
+            this.Name = "Form1";
+            this.Text = "Atividade 4";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtLadoA;
+        private System.Windows.Forms.TextBox txtLadoB;
+        private System.Windows.Forms.TextBox txtLadoC;
+        private System.Windows.Forms.Label lblLadoA;
+        private System.Windows.Forms.Label lblLadoB;
+        private System.Windows.Forms.Label lblLadoC;
+        private System.Windows.Forms.Button btnCalcular;
+        private System.Windows.Forms.Button btnLimpar;
+        private System.Windows.Forms.Button btnSair;
+    }
+}
+
---
+++ atividade4/atividade4/atividade4/Form1.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace atividade4
+{
+    public partial class Form1 : Form
+    {
+        double LadoA, LadoB, LadoC;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnSair_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void txtLadoB_Validated(object sender, EventArgs e)
+        {
+            if (!double.TryParse(txtLadoB.Text, out LadoB))
+                MessageBox.Show("numero invalido");
+            else if (LadoB < 0)
+                MessageBox.Show("Numero invalido");
+        }
+
+        private void txtLadoC_Validated(object sender, EventArgs e)
+        {
+            if (!double.TryParse(txtLadoC.Text, out LadoC))
+                MessageBox.Show("numero invalido");
+            else if (LadoC < 0)
+                MessageBox.Show("numero invalido");
+        }
+
+        private void btnCalcular_Click(object sender, EventArgs e)
+        {
+            if ((LadoA < (LadoB + LadoC)) && (LadoA > Math.Abs(LadoB - LadoC)) &&
+                 (LadoB < (LadoA + LadoC)) && (LadoB > Math.Abs(LadoA - LadoC)) &&
+                 (LadoC < (LadoB + LadoA)) && (LadoC > Math.Abs(LadoB - LadoA)) &&
+                 (LadoA == LadoB) && (LadoB == LadoC))
+            {
+                MessageBox.Show("É triângulo,e é equilátero");
+            }
+            else if ((LadoA < (LadoB + LadoC)) && (LadoA > Math.Abs(LadoB - LadoC)) &&
+                (LadoB < (LadoA + LadoC)) && (LadoB > Math.Abs(LadoA - LadoC)) &&
+                (LadoC < (LadoB + LadoA)) && (LadoC > Math.Abs(LadoB - LadoA)) &&
+                (LadoA == LadoB) || (LadoA == LadoC) || (LadoB == LadoC))
+            {
+                MessageBox.Show("É triângulo,e é isósceles");
+            }
+            else
+            {
+                MessageBox.Show("É triângulo,e é escaleno");
+            }
+        }
+            private void btnLimpar_Click(object sender, EventArgs e)
+            {
+                txtLadoA.Clear();
+                txtLadoB.Clear();
+                txtLadoC.Clear();
+                
+            }
+
+            private void txtLadoA_Validated(object sender, EventArgs e)
+            {
+                if (!double.TryParse(txtLadoA.Text, out LadoA))
+                    MessageBox.Show("Numero invalido");
+                else if (LadoA < 0) ;
+                MessageBox.Show("numero invalido");
+            }
+        }
+    
+}    
