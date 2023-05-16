d30c3b0b15773495dee153b7b967b48c737822f4
David Aurelio gabilan Junior <87284298+davidagj@users.noreply.github.com>
2023-05-16 11:02:07 -0300

    Add files via upload

---
+++ PROVA/PCotacao/PCotacao/Form1.Designer.cs
@@ -0,0 +1,73 @@
+﻿namespace PCotacao
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lstbS = new System.Windows.Forms.ListBox();
+            this.btnex = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lstbS
+            // 
+            this.lstbS.FormattingEnabled = true;
+            this.lstbS.Location = new System.Drawing.Point(229, 91);
+            this.lstbS.Name = "lstbS";
+            this.lstbS.Size = new System.Drawing.Size(546, 290);
+            this.lstbS.TabIndex = 0;
+            this.lstbS.SelectedIndexChanged += new System.EventHandler(this.LstbS_SelectedIndexChanged);
+            // 
+            // btnex
+            // 
+            this.btnex.Location = new System.Drawing.Point(41, 143);
+            this.btnex.Name = "btnex";
+            this.btnex.Size = new System.Drawing.Size(117, 118);
+            this.btnex.TabIndex = 1;
+            this.btnex.Text = "Executar";
+            this.btnex.UseVisualStyleBackColor = true;
+            this.btnex.Click += new System.EventHandler(this.Btnex_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnex);
+            this.Controls.Add(this.lstbS);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox lstbS;
+        private System.Windows.Forms.Button btnex;
+    }
+}
+
---
+++ PROVA/PCotacao/PCotacao/Form1.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Microsoft.VisualBasic;
+
+namespace PCotacao
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Btnex_Click(object sender, EventArgs e)
+        {
+            float[,] matriz = new float[7, 3];
+            string auxiliar = "";
+            float mediaG=0, media1=0, media2=0, media3=0, media4=0, media5=0, media6=0, media7=0;
+            for (int i = 0; i < 7; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    auxiliar = Interaction.InputBox(" digite o valor do notebook " , " Entrada de dados ");
+                    if (!float.TryParse(auxiliar, out matriz[i, j]) || matriz[i,j]<0)
+                    {
+                        MessageBox.Show("Valor invalido , digite novamente");
+                        i--;
+                        j--;
+                    }
+                }
+            }
+            for (int i = 0; i < 7; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    lstbS.Items.Add(" Notebook "+ (i+1) + " loja "+ (j+1)+ " R$ "+ matriz[i,j]);
+                   
+                        mediaG += matriz[i, j];
+                    media1 += matriz[0, j];
+                    media2 += matriz[1, j];
+                    media3 += matriz[2, j];
+                    media4 += matriz[3, j];
+                    media5 += matriz[4, j];
+                    media6 += matriz[5, j];
+                    media7 += matriz[6, j];
+
+                }
+            }
+            mediaG = mediaG / 21;
+            media1 = media1 / 3;
+            media2 = media2 / 3;
+            media3 = media3 / 3;
+            media4 = media4 / 3;
+            media5 = media5 / 3;
+            media6 = media6 / 3;
+            media7 = media7 / 3;
+            lstbS.Items.Add("Media computadores "+ mediaG);
+            lstbS.Items.Add("Media notebook 1 " +"media "+ media1);
+            lstbS.Items.Add("Media notebook 2 " + "media " + media2);
+            lstbS.Items.Add("Media notebook 3 " + "media " + media3);
+            lstbS.Items.Add("Media notebook 4 " + "media " + media4);
+            lstbS.Items.Add("Media notebook 5 " + "media " + media5);
+            lstbS.Items.Add("Media notebook 6 " + "media " + media6);
+            lstbS.Items.Add("Media notebook 7 " + "media " + media7);
+            
+        }
+        private void LstbS_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
